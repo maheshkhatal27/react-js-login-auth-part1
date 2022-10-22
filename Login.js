816aa8645d8eccc3ac79b46d1eb700fbc7b1f759
maheshkhatal27 <112764461+maheshkhatal27@users.noreply.github.com>
2022-10-22 11:40:34 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,121 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onLoginSuccess = () => {
+    const {history} = this.props
+    // in history there are push and replace methods using them we navigate to home
+
+    history.replace('/')
+    // we can use replace also to navigate to next route. But difference between push and replace is that
+    // in replace we can't go back and forth
+    // when to use replace or push. Obviously when once login, we don't need to go back to navigate,rather it can be
+    // used to navigate between different other web pages
+  }
+
+  onSubmitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    // console.log(data)
+    // if api call is successful then we will navigate to home-using history obj
+    //  history obj is present in props
+
+    console.log(response)
+    // in response there is ok which means call was successful.
+    // if call is a successful then we navigate to home route
+
+    if (response.ok === true) {
+      this.onLoginSuccess()
+    }
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.onSubmitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
